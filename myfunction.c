fdfab1aa80216ca646d7b998884f577999d35e03
lilach lacham <lilachlacham@gmail.com>
2022-01-07 12:30:55 -0800

    with: 58, no: 42

--- myfunction.c
+++ myfunction.c
@@ -62,27 +62,33 @@ static void assign_sum_to_pixel(pixel *current_pixel, pixel_sum sum, int kernelS
 * sum_pixels_by_weight - Sums pixel values, scaled by given weight
 */
 #define sum_pixels_by_weight(sum, p, weight) \
-(sum)->red += ((int) (p).red) * (weight); \
-(sum)->green += ((int) (p).green) * (weight);\
-(sum)->blue += ((int) (p).blue) * (weight);
+(sum).red += ((int) (p).red) * (weight); \
+(sum).green += ((int) (p).green) * (weight);\
+(sum).blue += ((int) (p).blue) * (weight);
 
+#define sum_pixels_by_color(color) \
+sum.color = (src[calcIndex(ii, jj, dim)].color * kernel1) + (src[calcIndex(ii, jj+1, dim)].color * kernel2) + \
+(src[calcIndex(ii, jj+2, dim)].color * kernel3) + (src[calcIndex(ii+1, jj, dim)].color * kernel4) + \
+(src[calcIndex(ii+1, jj+1, dim)].color * kernel5) + (src[calcIndex(ii+1, jj+2, dim)].color * kernel6) + \
+(src[calcIndex(ii+2, jj, dim)].color * kernel7) + (src[calcIndex(ii+2, jj+1, dim)].color * kernel8) + \
+(src[calcIndex(ii+2, jj+2, dim)].color * kernel9);
 
 /*
 * Apply the kernel over each pixel.
 * Ignore pixels where the kernel exceeds bounds. These are pixels with row index smaller than kernelSize/2 and/or
 * column index smaller than kernelSize/2
 */
-void smooth(int dim, pixel *src, pixel *dst, int kernel[KERNEL_SIZE][KERNEL_SIZE], int kernelScale, bool filter) {
+/*void smooth(int dim, pixel *src, pixel *dst, int kernel[KERNEL_SIZE][KERNEL_SIZE], int kernelScale, bool filter) {
 
-    int kernel1 = kernel[0][0];
-    int kernel2 = kernel[0][1];
-    int kernel3 = kernel[0][2];
-    int kernel4 = kernel[1][0];
-    int kernel5 = kernel[1][1];
-    int kernel6 = kernel[1][2];
-    int kernel7 = kernel[2][0];
-    int kernel8 = kernel[2][1];
-    int kernel9 = kernel[2][2];
+    register int kernel1 = kernel[0][0];
+    register int kernel2 = kernel[0][1];
+    register int kernel3 = kernel[0][2];
+    register int kernel4 = kernel[1][0];
+    register int kernel5 = kernel[1][1];
+    register int kernel6 = kernel[1][2];
+    register int kernel7 = kernel[2][0];
+    register int kernel8 = kernel[2][1];
+    register int kernel9 = kernel[2][2];
 
     register int i, j;
     register int startBound = KERNEL_SIZE / 2;
@@ -96,8 +102,8 @@ void smooth(int dim, pixel *src, pixel *dst, int kernel[KERNEL_SIZE][KERNEL_SIZE
             do {          \
             pixel_sum sum;\
             pixel current_pixel;\
-            register int min_intensity = 766; /* arbitrary value that is higher than maximum possible intensity, which is 255*3=765*/ \
-            register int max_intensity = -1; /* arbitrary value that is lower than minimum possible intensity, which is 0*/ \
+            register int min_intensity = 766; *//* arbitrary value that is higher than maximum possible intensity, which is 255*3=765*//* \
+            register int max_intensity = -1; *//* arbitrary value that is lower than minimum possible intensity, which is 0*//* \
             register int min_row, min_col, max_row, max_col; \
             pixel loop_pixel; \
             initialize_pixel_sum(&sum);\
@@ -113,7 +119,7 @@ void smooth(int dim, pixel *src, pixel *dst, int kernel[KERNEL_SIZE][KERNEL_SIZE
             sum_pixels_by_weight(&sum, src[calcIndex(ii+2, jj+1, dim)], kernel8); \
             sum_pixels_by_weight(&sum, src[calcIndex(ii+2, jj+2, dim)], kernel9); \
             if (filter) { \
-                /* find min and max coordinates*/ \
+                *//* find min and max coordinates*//* \
                 ii = max(i-1, 0); \
                 jj = max(j1-1,0); \
                 register int intensity = 0; \
@@ -126,11 +132,11 @@ void smooth(int dim, pixel *src, pixel *dst, int kernel[KERNEL_SIZE][KERNEL_SIZE
                 filterMacro(src, ii+2, jj,dim,min_intensity, min_row, min_col, max_intensity, max_row, max_col) \
                 filterMacro(src, ii+2, jj+1,dim,min_intensity, min_row, min_col, max_intensity, max_row, max_col) \
                 filterMacro(src, ii+2,jj+2,dim,min_intensity, min_row, min_col, max_intensity, max_row, max_col) \
-                /* filter out min and max*/ \
+                *//* filter out min and max*//* \
                 sum_pixels_by_weight(&sum, src[calcIndex(min_row, min_col, dim)], -1);\
                 sum_pixels_by_weight(&sum, src[calcIndex(max_row, max_col, dim)], -1);\
             }\
-            /* assign kernel's result to pixel at [i,j]*/ \
+            *//* assign kernel's result to pixel at [i,j]*//* \
             assign_sum_to_pixel(&current_pixel, sum, kernelScale);\
             dst[calcIndex(i, j1, dim)] = current_pixel;\
         } while(0);
@@ -160,6 +166,76 @@ void smooth(int dim, pixel *src, pixel *dst, int kernel[KERNEL_SIZE][KERNEL_SIZE
             smoothLoop(j);
         }
 	}
+}*/
+
+/*
+* Apply the kernel over each pixel.
+* Ignore pixels where the kernel exceeds bounds. These are pixels with row index smaller than kernelSize/2 and/or
+* column index smaller than kernelSize/2
+*/
+void smooth(int dim, pixel *src, pixel *dst, int kernel[KERNEL_SIZE][KERNEL_SIZE], int kernelScale, bool filter) {
+
+    register int kernel1 = kernel[0][0];
+    register int kernel2 = kernel[0][1];
+    register int kernel3 = kernel[0][2];
+    register int kernel4 = kernel[1][0];
+    register int kernel5 = kernel[1][1];
+    register int kernel6 = kernel[1][2];
+    register int kernel7 = kernel[2][0];
+    register int kernel8 = kernel[2][1];
+    register int kernel9 = kernel[2][2];
+
+    register int i, j;
+    register int start = KERNEL_SIZE / 2;
+    register int end = dim - start;
+    for (i = start ; i < end; i++) {
+        for (j =  start ; j < end ; j++) {
+            pixel_sum sum;
+            pixel current_pixel;
+            register int min_intensity = 766; // arbitrary value that is higher than maximum possible intensity, which is 255*3=765
+            register int max_intensity = -1; // arbitrary value that is lower than minimum possible intensity, which is 0
+            register int min_row, min_col, max_row, max_col;
+            pixel loop_pixel;
+
+            initialize_pixel_sum(&sum);
+            register int ii = max(i-1, 0);
+            register int jj = max(j-1, 0);
+
+            sum_pixels_by_color(red);
+            sum_pixels_by_color(green);
+            sum_pixels_by_color(blue);
+
+            if (filter) {
+                register int maxII = min(i+1, dim-1);
+                register int maxJJ = min(j+1, dim-1);
+                for(; ii <= maxII; ++ii) {
+                    for(jj = max(j-1, 0); jj <=maxJJ; ++jj) {
+                        // apply kernel on pixel at [ii,jj]
+                        loop_pixel = src[calcIndex(ii, jj, dim)];
+                        register int intensity = (int) loop_pixel.red + ((int) loop_pixel.green) + ((int) loop_pixel.blue);
+                        if (intensity <= min_intensity) {
+                            min_intensity = intensity;
+                            min_row = ii;
+                            min_col = jj;
+                        }
+                        if (intensity > max_intensity) {
+                            max_intensity = intensity;
+                            max_row = ii;
+                            max_col = jj;
+                        }
+                    }
+                }
+                // find min and max coordinates
+                // filter out min and max
+                sum_pixels_by_weight(sum, src[calcIndex(min_row, min_col, dim)], -1);
+                sum_pixels_by_weight(sum, src[calcIndex(max_row, max_col, dim)], -1);
+            }
+
+            // assign kernel's result to pixel at [i,j]
+            assign_sum_to_pixel(&current_pixel, sum, kernelScale);
+            dst[calcIndex(i, j, dim)] = current_pixel;
+        }
+    }
 }
 
 /*void charsToPixels(pixel* pixels) {
