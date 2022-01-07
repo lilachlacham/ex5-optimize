f5385e2476c7743121824052f25c822213fee5d3
lilach lacham <lilachlacham@gmail.com>
2022-01-07 07:41:00 -0800

    with: 63, no: 49

--- myfunction.c
+++ myfunction.c
@@ -1,4 +1,6 @@
 #include <stdbool.h> 
+#define KERNEL_SIZE 3
+
 
 typedef struct {
    unsigned char red;
@@ -70,11 +72,7 @@ static void assign_sum_to_pixel(pixel *current_pixel, pixel_sum sum, int kernelS
 * Ignore pixels where the kernel exceeds bounds. These are pixels with row index smaller than kernelSize/2 and/or
 * column index smaller than kernelSize/2
 */
-void smooth(int dim, pixel *src, pixel *dst, int kernelSize, int kernel[kernelSize][kernelSize], int kernelScale, bool filter) {
-
-	int i, j;
-    int startBound = kernelSize / 2;
-    int endBound = dim - startBound;
+void smooth(int dim, pixel *src, pixel *dst, int kernel[KERNEL_SIZE][KERNEL_SIZE], int kernelScale, bool filter) {
 
     int kernel1 = kernel[0][0];
     int kernel2 = kernel[0][1];
@@ -86,21 +84,25 @@ void smooth(int dim, pixel *src, pixel *dst, int kernelSize, int kernel[kernelSi
     int kernel8 = kernel[2][1];
     int kernel9 = kernel[2][2];
 
-    int carry = endBound % 20;
+    register int i, j;
+    register int startBound = KERNEL_SIZE / 2;
+    register int endBoundI = dim - startBound;
+    register int carry = endBoundI % 20;
+    register int endBoundJ = endBoundI - carry;
 
-	for (i = startBound ; i < endBound; ++i) {
-		for (j =  startBound ; j < endBound-carry ; j+=20) {
+	for (i = startBound ; i < endBoundI; ++i) {
+		for (j =  startBound ; j < endBoundJ ; j+=20) {
 #define smoothLoop(j1) \
             do {          \
             pixel_sum sum;\
             pixel current_pixel;\
-            int min_intensity = 766; /* arbitrary value that is higher than maximum possible intensity, which is 255*3=765*/ \
-            int max_intensity = -1; /* arbitrary value that is lower than minimum possible intensity, which is 0*/ \
-            int min_row, min_col, max_row, max_col; \
+            register int min_intensity = 766; /* arbitrary value that is higher than maximum possible intensity, which is 255*3=765*/ \
+            register int max_intensity = -1; /* arbitrary value that is lower than minimum possible intensity, which is 0*/ \
+            register int min_row, min_col, max_row, max_col; \
             pixel loop_pixel; \
             initialize_pixel_sum(&sum);\
-            int ii = max(i-1, 0);\
-            int jj = max(j1-1, 0);\
+            register int ii = max(i-1, 0);\
+            register int jj = max(j1-1, 0);\
             sum_pixels_by_weight(&sum, src[calcIndex(ii, jj, dim)], kernel1); \
             sum_pixels_by_weight(&sum, src[calcIndex(ii, jj+1, dim)], kernel2); \
             sum_pixels_by_weight(&sum, src[calcIndex(ii, jj+2, dim)], kernel3); \
@@ -114,7 +116,7 @@ void smooth(int dim, pixel *src, pixel *dst, int kernelSize, int kernel[kernelSi
                 /* find min and max coordinates*/ \
                 ii = max(i-1, 0); \
                 jj = max(j1-1,0); \
-                int intensity = 0; \
+                register int intensity = 0; \
                 filterMacro(src, ii, jj,dim,min_intensity, min_row, min_col, max_intensity, max_row, max_col) \
                 filterMacro(src, ii, jj+1,dim,min_intensity, min_row, min_col, max_intensity, max_row, max_col) \
                 filterMacro(src, ii, jj+2,dim,min_intensity, min_row, min_col, max_intensity, max_row, max_col) \
@@ -154,7 +156,7 @@ void smooth(int dim, pixel *src, pixel *dst, int kernelSize, int kernel[kernelSi
             smoothLoop(j+18);
             smoothLoop(j+19);
 		}
-        for (;j < endBound;++j) {
+        for (;j < endBoundI;++j) {
             smoothLoop(j);
         }
 	}
@@ -162,11 +164,11 @@ void smooth(int dim, pixel *src, pixel *dst, int kernelSize, int kernel[kernelSi
 
 void charsToPixels(pixel* pixels) {
 
-	int row, col;
+	register int row, col;
 	for (row = 0 ; row < m ; ++row) {
 		for (col = 0 ; col < n ; ++col) {
-            unsigned int row_n_col = row*n + col;
-            unsigned int row_n_col_3 = 3*row_n_col;
+            register unsigned int row_n_col = row*n + col;
+            register unsigned int row_n_col_3 = 3*row_n_col;
             pixels[row_n_col] = *(pixel*)&(image->data[row_n_col_3]);
 		}
 	}
@@ -177,8 +179,8 @@ void pixelsToChars(pixel* pixels) {
 	int row, col;
 	for (row = 0 ; row < m ; ++row) {
 		for (col = 0 ; col < n ; ++col) {
-            unsigned int row_n_col = row*n + col;
-            unsigned int row_n_col_3 = 3*row_n_col;
+            register unsigned int row_n_col = row*n + col;
+            register unsigned int row_n_col_3 = 3*row_n_col;
             *(pixel *)&(image->data[row_n_col_3]) = pixels[row_n_col];
 		}
 	}
@@ -189,13 +191,13 @@ void copyPixels(pixel* src, pixel* dst) {
 	int row, col;
 	for (row = 0 ; row < m ; ++row) {
 		for (col = 0 ; col < n ; ++col) {
-            unsigned int row_n_col = row*n + col;
+            register unsigned int row_n_col = row*n + col;
             dst[row_n_col] = src[row_n_col];
 		}
 	}
 }
 
-void doConvolution(int kernelSize, int kernel[kernelSize][kernelSize], int kernelScale, bool filter) {
+void doConvolution(int kernel[KERNEL_SIZE][KERNEL_SIZE], int kernelScale, bool filter) {
 
 	pixel* pixelsImg = malloc(m*n*sizeof(pixel));
 	pixel* backupOrg = malloc(m*n*sizeof(pixel));
@@ -203,7 +205,7 @@ void doConvolution(int kernelSize, int kernel[kernelSize][kernelSize], int kerne
 	charsToPixels(pixelsImg);
 	copyPixels(pixelsImg, backupOrg);
 
-	smooth(m, backupOrg, pixelsImg, kernelSize, kernel, kernelScale, filter);
+	smooth(m, backupOrg, pixelsImg, kernel, kernelScale, filter);
 
 	pixelsToChars(pixelsImg);
 
@@ -229,25 +231,25 @@ void myfunction(Image *image, char* srcImgpName, char* blurRsltImgName, char* sh
 
 	if (flag == '1') {	
 		// blur image
-		doConvolution(3, blurKernel, 9, false);
+		doConvolution(blurKernel, 9, false);
 
 		// write result image to file
 		writeBMP(image, srcImgpName, blurRsltImgName);	
 
 		// sharpen the resulting image
-		doConvolution(3, sharpKernel, 1, false);
+		doConvolution(sharpKernel, 1, false);
 		
 		// write result image to file
 		writeBMP(image, srcImgpName, sharpRsltImgName);	
 	} else {
 		// apply extermum filtered kernel to blur image
-		doConvolution(3, blurKernel, 7, true);
+		doConvolution(blurKernel, 7, true);
 
 		// write result image to file
 		writeBMP(image, srcImgpName, filteredBlurRsltImgName);
 
 		// sharpen the resulting image
-		doConvolution(3, sharpKernel, 1, false);
+		doConvolution(sharpKernel, 1, false);
 
 		// write result image to file
 		writeBMP(image, srcImgpName, filteredSharpRsltImgName);	
